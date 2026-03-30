0376e3854ebb9366f8a718f1f0ecbb3ba131b17a
FranGuelfo <fran.guelfo@gmail.com>
2026-03-30 12:08:16 +0200

    feat: first commit

---
+++ src/main/java/com/wefly/wefly/WeflyApplication.java
@@ -0,0 +1,13 @@
+package com.wefly.wefly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeflyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wefly/wefly/WeflyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wefly.wefly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeflyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
